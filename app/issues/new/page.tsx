6c16d73f8e3dc680bc449399155ff42852461685
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-15 19:14:28 +0300

    Refactoring import statements

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+import { ErrorMessage, Spinner } from "@/app/components";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import z from "zod";
-import dynamic from "next/dynamic";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,10 @@
-import Link from "@/app/components/Link";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import delay from "delay";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import { IssueStatusBadge, Link } from "@/app/components";
 import IssueActions from "./IssueActions";
 
 export default async function IssuesPage() {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
 
   return (
     <div>
@@ -16,10 +13,10 @@ export default async function IssuesPage() {
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
+            <Table.ColumnHeaderCell className="hidden sm:table-cell">
               Status
             </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
+            <Table.ColumnHeaderCell className="hidden sm:table-cell">
               Created
             </Table.ColumnHeaderCell>
           </Table.Row>
@@ -29,14 +26,14 @@ export default async function IssuesPage() {
             <Table.Row key={issue.id}>
               <Table.Cell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
+                <div className="block sm:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
+              <Table.Cell className="hidden sm:table-cell">
                 <IssueStatusBadge status={issue.status} />
               </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
+              <Table.Cell className="hidden sm:table-cell">
                 {issue.createdAt.toDateString()}
               </Table.Cell>
             </Table.Row>
