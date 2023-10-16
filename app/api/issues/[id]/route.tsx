1a21e0035fada4dfc93c79b2d423688adbc90469
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-16 20:33:59 +0300

    Improve user deleting experience

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
 import { IssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
 interface ParamsProps {
   params: { id: string };
--- app/issues/[id]/IssueDeleteButton.tsx
+++ app/issues/[id]/IssueDeleteButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -7,13 +8,16 @@ import { useState } from "react";
 export default function IssueDeleteButton({ issueId }: { issueId: number }) {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
 
   async function handleDelete() {
     try {
+      setDeleting(true);
       await axios.delete(`/api/issues/${issueId}`);
       router.push("/issues");
       router.refresh();
     } catch (error) {
+      setDeleting(false);
       setError(true);
     }
   }
@@ -22,7 +26,10 @@ export default function IssueDeleteButton({ issueId }: { issueId: number }) {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete issue</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete issue
+            {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
 
         <AlertDialog.Content>
