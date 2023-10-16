311f0ec2ec66bf5445a18e79fdb64e268b51580e
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-16 15:20:39 +0300

    Import Issue Form loading

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,12 +1,18 @@
 import prisma from "@/prisma/client";
+import dynamic from "next/dynamic";
 import { notFound } from "next/navigation";
-import IssueForm from "../../_components/IssueForm";
+import IssueFormSkeleton from "./loading";
 
 interface Props {
   params: {
     id: string;
   };
 }
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 export default async function EditIssuePage({ params: { id } }: Props) {
   const issue = await prisma.issue.findUnique({
