a514d5baa5f03711523e1804be5e5cbc6a2d1403
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-17 14:58:08 +0300

    Refactor nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,68 +13,82 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
-export default function NavBar() {
+function NavLinks() {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues/list" },
   ];
   const currentPath = usePathname();
+  return (
+    <Flex gap="3" align="center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Flex>
+  );
+}
+
+function AuthStatus() {
   const { data: session, status } = useSession();
 
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated") return;
+  <Button>
+    <Link href="/api/auth/signin">Log in</Link>
+  </Button>;
+
+  return (
+    <Box>
+      {status === "authenticated" && (
+        <>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Avatar
+                src={session.user!.image!}
+                fallback="?"
+                radius="full"
+                className="cursor-pointer"
+                referrerPolicy="no-referrer"
+              />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Label>
+                <Text>{session.user!.email}</Text>
+              </DropdownMenu.Label>
+              <DropdownMenu.Item>
+                <Link href="/api/auth/signout">Log out</Link>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </>
+      )}
+    </Box>
+  );
+}
+
+export default function NavBar() {
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Flex justify="between">
-        <Flex gap="3" align="center">
-          <Link href="/">
-            <AiFillBug />
-          </Link>
-          <ul className="flex space-x-6">
-            {links.map((link) => (
-              <li key={link.href}>
-                <Link
-                  className={classNames({
-                    "text-zinc-900": link.href === currentPath,
-                    "text-zinc-500": link.href !== currentPath,
-                    "hover:text-zinc-800 transition-colors": true,
-                  })}
-                  href={link.href}
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </Flex>
-        <Box>
-          {status === "authenticated" && (
-            <>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text>{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Log out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            </>
-          )}
-          {status === "unauthenticated" && (
-            <Button>
-              <Link href="/api/auth/signin">Log in</Link>
-            </Button>
-          )}
-        </Box>
+        <NavLinks />
+        <AuthStatus />
       </Flex>
     </nav>
   );
