bcc5d06f0ef810fd25aaad0fcc84e28893786a3c
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-19 15:42:10 +0300

    Create pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Pagination from "./components/pagination";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+    </div>
+  );
 }
