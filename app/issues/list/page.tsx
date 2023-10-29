2888b592a5ce11b03d046bf0ffaa7d816e7d8bf6
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-10-29 14:43:46 +0200

    Show only user`s issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,18 +5,26 @@ import { Flex } from "@radix-ui/themes";
 import { Metadata } from "next";
 import IssueActions from "./IssueActions";
 import IssueTable, { IssueQuery, columnNames } from "./IssueTable";
+import { getServerSession } from "next-auth";
 
 interface Props {
   searchParams: IssueQuery;
 }
 
 export default async function IssuesPage({ searchParams }: Props) {
+  const session = await getServerSession();
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
-  const where = { status };
+  const currentUser = session?.user
+    ? await prisma.user.findUnique({
+        where: { email: session?.user?.email! },
+      })
+    : undefined;
+
+  const where = { status, assignedToUserId: currentUser?.id };
 
   const orderBy =
     columnNames.includes(searchParams.orderBy) || searchParams.order
@@ -26,12 +34,14 @@ export default async function IssuesPage({ searchParams }: Props) {
   const page = Number(searchParams.page) || 1;
   const pageSize = 10;
 
-  const issues = await prisma.issue.findMany({
-    where,
-    orderBy,
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-  });
+  const issues = currentUser
+    ? await prisma.issue.findMany({
+        where,
+        orderBy,
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+      })
+    : [];
 
   const totalIssues = await prisma.issue.count({
     where,
