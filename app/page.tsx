73397fbe352ad0510e5c3762059572eb75a540c5
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-12-24 14:50:24 +0200

    Add error logging

--- app/page.tsx
+++ app/page.tsx
@@ -6,21 +6,31 @@ import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
-  const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inProgress = await prisma.issue.count({
-    where: { status: "IN_PROGRESS" },
-  });
-  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  try {
+    const open = await prisma.issue.count({ where: { status: "OPEN" } });
+    const inProgress = await prisma.issue.count({
+      where: { status: "IN_PROGRESS" },
+    });
+    const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+    const all = await prisma.issue.findMany();
 
-  return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Flex direction="column" gap="5">
-        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
-        <IssueChart open={open} inProgress={inProgress} closed={closed} />
-      </Flex>
-      <LatestIssues />
-    </Grid>
-  );
+    console.log("Open", open);
+    console.log("Progress", inProgress);
+    console.log("Closed", closed);
+    console.log("All", all);
+
+    return (
+      <Grid columns={{ initial: "1", md: "2" }} gap="5">
+        <Flex direction="column" gap="5">
+          <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+          <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        </Flex>
+        <LatestIssues />
+      </Grid>
+    );
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const metadata: Metadata = {
